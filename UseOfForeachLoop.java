9814af3ac4ac81a025e534e36a7e6335889ebb14
Md. Talal Wasim <91146041+mdtalalwasim@users.noreply.github.com>
2022-10-02 20:36:21 +0600

    Java code for all
    Some operation of array, used - For each loop.

---
+++ UseOfForeachLoop.java
@@ -0,0 +1,46 @@
+
+public class UseOfForeachLoop {
+
+    ///*
+    public static void main(String[] args) {
+
+       //use of foreach Loop...
+
+       int a[];
+       a= new int[2];
+       a[0] = 2;
+       a[1] = 5;
+        for (int b: a
+             ) {
+            System.out.println(b);
+
+        }
+
+        int a2[] = new int[]{2,5,7,8,9,10,50};
+        for (int i: a2
+             ) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+        int a3[] = {2,5,5,7};
+        for (int arr: a3
+             ) {
+            System.out.print(arr+" ");
+        }
+        System.out.println();
+
+
+        String str[] = {"Wasim Ahmed","Taimur Ahmed", "Abdur Rahman Nahi", "Abdullah Aiyaan"};
+        for (String st: str
+             ) {
+            System.out.println(st);
+        }
+
+    }
+    //*/
+
+
+}
+
+
